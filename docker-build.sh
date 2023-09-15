c39e213254090e17900adcf2591b2ef773e3977b
Lenny Andreu <landreuhs.29@gmail.com>
2023-09-15 16:17:23 -0300

    Update dockerfile to build nsis
    Also add exception to prevent upload build dir
    and 7kaa.exe generated file

--- docker-build.sh
+++ docker-build.sh
@@ -3,11 +3,9 @@ CONTAINER_VERSION=dev
 CONTAINER=${CONTAINER_NAME}:${CONTAINER_VERSION}
 
 FILE=$1 || dockerfile
-echo $FILE
+echo Building for $FILE
 # CONTAINER_TAG_NAME=$(echo $FILE | awk '{print $2$3}' FS='.')
 docker build . -t ${CONTAINER} -f $FILE && \
 docker cp \
-  $(docker create --rm $CONTAINER):/app/ \
-. && \
-#rm -rf app/dest/
-ls app/dest
+  $(docker create --rm $CONTAINER):/app/src/7kaa \
+.
